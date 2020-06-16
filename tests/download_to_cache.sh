539fdb38f9719e9d422428a67ebb5164e3e7d0a0
public-tatsuya-noyori <wsl@DESKTOP-7JDGS07.localdomain>
2020-06-16 18:23:54 +0900

    26th commit.

--- tests/download_to_cache.sh
+++ tests/download_to_cache.sh
@@ -18,22 +18,22 @@
 #   Tatsuya Noyori - Japan Meteorological Agency - https://www.jma.go.jp
 #
 set -ex
-is_download_wis_jma_to_cache_running=`touch download_wis_jma_to_cache.pid && cat download_wis_jma_to_cache.pid | xargs -n1 -I{} ps -h -q {} | wc -l`
+is_download_wis_jma_to_cache_running=`touch download_wis_jma_to_cache.pid && cat download_wis_jma_to_cache.pid | xargs -I{} ps --no-headers  -q {} | wc -l`
 if test ${is_download_wis_jma_to_cache_running} -eq 0; then
     ./download_wis_jma_to_cache.sh > download_wis_jma_to_cache_list_file.txt &
     echo $! > download_wis_jma_to_cache.pid
 fi
-is_download_noaa_batch_to_cache_running=`touch download_noaa_batch_to_cache.pid && cat download_noaa_batch_to_cache.pid | xargs -n1 -I{} ps -h -q {} | wc -l`
-if test ${is_download_noaa_batch_to_cache_running} -eq 0; then
-    ./download_noaa_batch_to_cache.sh > download_noaa_batch_to_cache_list_file.txt &
-    echo $! > download_noaa_batch_to_cache.pid
-fi
-is_upload_to_object_storage_running=`touch upload_to_object_storage.pid && cat upload_to_object_storage.pid  | xargs -n1 -I{} ps -h -q {} | wc -l`
+#is_download_noaa_batch_to_cache_running=`touch download_noaa_batch_to_cache.pid && cat download_noaa_batch_to_cache.pid | xargs -I{} ps --no-headers -q {} | wc -l`
+#if test ${is_download_noaa_batch_to_cache_running} -eq 0; then
+#    ./download_noaa_batch_to_cache.sh > download_noaa_batch_to_cache_list_file.txt &
+#    echo $! > download_noaa_batch_to_cache.pid
+#fi
+is_upload_to_object_storage_running=`touch upload_to_object_storage.pid && cat upload_to_object_storage.pid  | xargs -I{} ps --no-headers -q {} | wc -l`
 if test ${is_upload_to_object_storage_running} -eq 0; then
     ./rclone --ignore-checksum --ignore-existing --max-age 1d --max-size 1G --no-update-modtime --stats 0 --timeout 1m --transfers 32 --s3-upload-concurrency 32 --use-server-modtime -u copy work.tmp/cache/open iij1:japan-meteorological-agency-open-data --log-level ERROR --log-file upload_to_object_storage.log &
     echo $! > upload_to_object_storage.pid
 fi
-is_delete_object_storage_running=`touch delete_object_storage.pid && cat delete_object_storage.pid  | xargs -n1 -I{} ps -h -q {} | wc -l`
+is_delete_object_storage_running=`touch delete_object_storage.pid && cat delete_object_storage.pid  | xargs -I{} ps --no-headers -q {} | wc -l`
 if test ${is_delete_object_storage_running} -eq 0; then
     ./rclone delete --min-age 1d --use-server-modtime iij1:japan-meteorological-agency-open-data/ --log-level ERROR --log-file delete_object_storage.log &
     echo $! > delete_object_storage.pid
