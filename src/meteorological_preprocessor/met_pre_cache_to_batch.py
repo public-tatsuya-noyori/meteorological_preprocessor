cb7c5ed772687183d765eb55f73126947fc13eda
public.tatsuya.noyori <public.tatsuya.noyori@gmail.com>
2021-01-14 11:30:10 +0900

    test code

--- src/meteorological_preprocessor/met_pre_cache_to_batch.py
+++ src/meteorological_preprocessor/met_pre_cache_to_batch.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright 2020 Japan Meteorological Agency.
+# Copyright 2020-2021 Japan Meteorological Agency.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
