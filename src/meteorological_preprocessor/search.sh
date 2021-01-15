1ef1dcb4f3837c55c9bad928980294ad722e66fb
public.tatsuya.noyori <public.tatsuya.noyori@gmail.com>
2021-01-15 17:03:04 +0900

    commit

--- src/meteorological_preprocessor/search.sh
+++ src/meteorological_preprocessor/search.sh
@@ -21,7 +21,7 @@ set -e
 for arg in "$@"; do
   case "${arg}" in
     "--debug" ) set -evx;shift;;
-    "--help" ) echo "$0 rclone_remote_bucket priority keyword [end_published_yyyymmddhh] [start_published_yyyymmddhh]"; exit 0;;
+    "--help" ) echo "$0 rclone_remote_bucket priority keyword [start_published_yyyymmddhh] [end_published_yyyymmddhh]"; exit 0;;
   esac
 done
 if test -z $3; then
@@ -35,52 +35,52 @@ if test -z ${priority}; then
   exit 199
 fi
 keyword=$3
-end_yyyymmddhh=0
+start_yyyymmddhh=0
 if test -n "$4"; then
-  end_yyyymmddhh=`echo $4 | grep "^[0-9]\+$"`
-  if test -z ${end_yyyymmddhh}; then
+  start_yyyymmddhh=`echo $4 | grep "^[0-9]\+$"`
+  if test -z ${start_yyyymmddhh}; then
     echo "ERROR: $4 is not integer." >&2
     exit 199
   elif test $4 -le 0; then
     echo "ERROR: $4 is not more than 1." >&2
     exit 199
   fi
-  end_yyyymmddhh=$4
+  start_yyyymmddhh=`expr 0 + $4`
 fi
-start_yyyymmddhh=0
+end_yyyymmddhh=0
 if test -n "$5"; then
-  start_yyyymmddhh=`echo $5 | grep "^[0-9]\+$"`
-  if test -z ${start_yyyymmddhh}; then
+  end_yyyymmddhh=`echo $5 | grep "^[0-9]\+$"`
+  if test -z ${end_yyyymmddhh}; then
     echo "ERROR: $5 is not integer." >&2
     exit 199
   elif test $5 -le 0; then
     echo "ERROR: $5 is not more than 1." >&2
     exit 199
   fi
-  start_yyyymmddhh=$5
+  end_yyyymmddhh=`expr 0 + $5`
 fi
-if test ${start_yyyymmddhh} -eq 0; then
-  if test ${end_yyyymmddhh} -eq 0; then
-    for index_directory in `rclone lsf ${rclone_remote_bucket}/4Search/${priority} | tac`; do
+if test ${end_yyyymmddhh} -eq 0; then
+  if test ${start_yyyymmddhh} -eq 0; then
+    for index_directory in `rclone lsf ${rclone_remote_bucket}/4Search/${priority}`; do
       yyyymmddhh=`echo ${index_directory} | cut -c1-10`
       yyyymmddhh=`expr 0 + ${yyyymmddhh}`
-      rclone lsf ${rclone_remote_bucket}/4Search/${priority}/${yyyymmddhh} | tac | xargs -r -n 1 -I {} rclone cat ${rclone_remote_bucket}/4Search/${priority}/${yyyymmddhh}/{} | grep ${keyword} | tac
+      rclone lsf ${rclone_remote_bucket}/4Search/${priority}/${yyyymmddhh} | xargs -r -n 1 -I {} rclone cat ${rclone_remote_bucket}/4Search/${priority}/${yyyymmddhh}/{} | grep ${keyword}
     done
   else
-    for index_directory in `rclone lsf ${rclone_remote_bucket}/4Search/${priority} | tac`; do
+    for index_directory in `rclone lsf ${rclone_remote_bucket}/4Search/${priority}`; do
       yyyymmddhh=`echo ${index_directory} | cut -c1-10`
       yyyymmddhh=`expr 0 + ${yyyymmddhh}`
-      if test ${yyyymmddhh} -le ${end_yyyymmddhh}; then
-        rclone lsf ${rclone_remote_bucket}/4Search/${priority}/${yyyymmddhh} | tac | xargs -r -n 1 -I {} rclone cat ${rclone_remote_bucket}/4Search/${priority}/${yyyymmddhh}/{} | grep ${keyword} | tac
+      if test ${yyyymmddhh} -ge ${start_yyyymmddhh}; then
+        rclone lsf ${rclone_remote_bucket}/4Search/${priority}/${yyyymmddhh} | xargs -r -n 1 -I {} rclone cat ${rclone_remote_bucket}/4Search/${priority}/${yyyymmddhh}/{} | grep ${keyword}
       fi
     done
   fi
 else
-  for index_directory in `rclone lsf ${rclone_remote_bucket}/4Search/${priority} | tac`; do
+  for index_directory in `rclone lsf ${rclone_remote_bucket}/4Search/${priority}`; do
     yyyymmddhh=`echo ${index_directory} | cut -c1-10`
     yyyymmddhh=`expr 0 + ${yyyymmddhh}`
     if test ${yyyymmddhh} -le ${end_yyyymmddhh} -a ${yyyymmddhh} -ge ${start_yyyymmddhh}; then
-      rclone lsf ${rclone_remote_bucket}/4Search/${priority}/${yyyymmddhh} | tac | xargs -r -n 1 -I {} rclone cat ${rclone_remote_bucket}/4Search/${priority}/${yyyymmddhh}/{} | grep ${keyword} | tac
+      rclone lsf ${rclone_remote_bucket}/4Search/${priority}/${yyyymmddhh} | xargs -r -n 1 -I {} rclone cat ${rclone_remote_bucket}/4Search/${priority}/${yyyymmddhh}/{} | grep ${keyword}
     fi
   done
 fi
