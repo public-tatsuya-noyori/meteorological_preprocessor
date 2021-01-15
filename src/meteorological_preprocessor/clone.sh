899aacbb4b4876428be0c29765fd4aad5bc716a7
public.tatsuya.noyori <public.tatsuya.noyori@gmail.com>
2021-01-15 11:26:35 +0900

    test code

--- src/meteorological_preprocessor/clone.sh
+++ src/meteorological_preprocessor/clone.sh
@@ -159,9 +159,9 @@ clone() {
                 sed -e "s|^|${date_hour_directory}|g" ${source_work_directory}/${priority}_${search_index_directory}_minute_second_index.tmp > ${source_work_directory}/${priority}_${search_index_directory}_index.tmp
                 former_index_file_first_line_count=`grep ${former_index_file_first_line} ${source_work_directory}/${priority}_${search_index_directory}_index.tmp | wc -l`
                 if test ${former_index_file_first_line_count} -eq 0; then
-                  grep -v -f ${source_work_directory}/${priority}_${pubsub_index_directory}_gotten_new_index.tmp ${source_work_directory}/${priority}_${search_index_directory}_index.tmp >> ${source_work_directory}/${priority}_${search_index_directory}_new_index.tmp
+                  grep -v -f ${source_work_directory}/${priority}_${pubsub_index_directory}_index.txt ${source_work_directory}/${priority}_${search_index_directory}_index.tmp | grep -v -f ${source_work_directory}/${priority}_${pubsub_index_directory}_gotten_new_index.tmp >> ${source_work_directory}/${priority}_${search_index_directory}_new_index.tmp
                 else
-                  sed -ne "/${former_index_file_first_line}/,\$p" ${source_work_directory}/${priority}_${search_index_directory}_index.tmp | grep -v -f ${source_work_directory}/${priority}_${pubsub_index_directory}_gotten_new_index.tmp >> ${source_work_directory}/${priority}_${search_index_directory}_new_index.tmp
+                  sed -ne "/${former_index_file_first_line}/,\$p" ${source_work_directory}/${priority}_${search_index_directory}_index.tmp | grep -v -f ${source_work_directory}/${priority}_${pubsub_index_directory}_index.txt | grep -v -f ${source_work_directory}/${priority}_${pubsub_index_directory}_gotten_new_index.tmp >> ${source_work_directory}/${priority}_${search_index_directory}_new_index.tmp
                   break
                 fi
               done
