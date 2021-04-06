2877bf513b6e8fffe022ae53430b9fe320c607f1
public.tatsuya.noyori <public.tatsuya.noyori@gmail.com>
2021-04-06 15:58:41 +0900

    c

--- src/meteorological_preprocessor/met_pre_arrow_to_tile_arrow.py
+++ src/meteorological_preprocessor/met_pre_arrow_to_tile_arrow.py
@@ -21,13 +21,14 @@
 import argparse
 import math
 import os
+import pkg_resources
 import pyarrow as pa
 import pandas as pd
 import re
 import sys
 import traceback
 from datetime import datetime, timedelta, timezone
-from pyarrow import feather
+from pyarrow import csv, feather
 
 def convert_to_tile_arrow(in_file_list, out_dir, zoom, out_list_file, conf_df, debug):
     warno = 189
@@ -43,20 +44,26 @@ def convert_to_tile_arrow(in_file_list, out_dir, zoom, out_list_file, conf_df, d
     new_id_etfo_dict = {}
     del_etfo_id_dict = {}
     out_file_dict = {}
-    for in_file in in_file_list:
-        if debug:
-            print('Debug', ': in_file', in_file, file=sys.stderr)
-        loc_time_match = re.search(r'^.*/([A-Z][A-Z][A-Z][A-Z])/([^/]*)/(.*)/C_([A-Z]{4})_([0-9]*)\.feather$', in_file)
-        if loc_time_match:
-            cccc = loc_time_match.group(1)
-            form = loc_time_match.group(2)
-            cat_dir = loc_time_match.group(3)
-            creator = loc_time_match.group(4)
-            created = loc_time_match.group(5)
+    convert_cat_subcat_set = set([re.sub(r'/C_[A-Z]{4}_[0-9]*\.feather$', '', re.search(r'[^/]*/[^/]*/[^/]*/[^/]*\.feather$', in_file).group()) for in_file in in_file_list])
+    for convert_cat_subcat in convert_cat_subcat_set:
+        convert_cat_subcat_match = re.search(r'^([^/]*)/([^/]*)/([^/]*)$', convert_cat_subcat)
+        convert = convert_cat_subcat_match.group(1)
+        cat = convert_cat_subcat_match.group(2)
+        subcat = convert_cat_subcat_match.group(3)
+        convert_cat_subcat_df = conf_df[(conf_df['convert'] == convert) & (conf_df['category'] == cat) & (conf_df['subcategory'] == subcat)]
+        for in_file in in_file_list:
+            match = re.search(r'^.*/([A-Z][A-Z][A-Z][A-Z])/' + convert_cat_subcat+ '/C_([A-Z]{4})_([0-9]*)\.feather$', in_file)
+            if not match:
+                continue
+            if debug:
+                print('Debug', ': in_file', in_file, file=sys.stderr)
+            cccc = match.group(1)
+            creator = match.group(2)
+            created = match.group(3)
             created_second = int(math.floor(datetime(int(created[0:4]), int(created[4:6]), int(created[6:8]), int(created[8:10]), int(created[10:12]), int(created[12:14]), 0, tzinfo=timezone.utc).timestamp()))
             new_datetime_list_dict = {}
             new_id_etfo_dict = {}
-            del_etfo_id_dict = {}
+            del_dict = {}
             in_df = feather.read_feather(in_file)
             for tile_x in range(0, 2**(zoom + 1)):
                 for tile_y in range(0, 2**(zoom)):
@@ -70,97 +77,58 @@ def convert_to_tile_arrow(in_file_list, out_dir, zoom, out_list_file, conf_df, d
                         tile_df = in_df[(res * tile_x - 180.0 < in_df['longitude [degree]']) & (in_df['longitude [degree]'] <= res * (tile_x + 1) - 180.0) & (90.0 - res * tile_y > in_df['latitude [degree]']) & (in_df['latitude [degree]'] >= 90.0 - res * (tile_y + 1))]
                     new_datetime_list_dict[tile_x,  tile_y] = tile_df['datetime'].dt.floor("10T").unique()
                     for new_datetime in new_datetime_list_dict[tile_x,  tile_y]:
-                        new_df = tile_df[(new_datetime <= tile_df['datetime']) & (tile_df['datetime'] < new_datetime + timedelta(minutes=10))]
-                        if len(new_df['id'].index) > 0:
-                            out_directory = ''.join([out_dir, '/', form, '/', cat_dir, '/', str(new_datetime.year).zfill(4), '/', str(new_datetime.month).zfill(2), str(new_datetime.day).zfill(2), '/', str(new_datetime.hour).zfill(2), str(math.floor(new_datetime.minute / 10)), '0/', str(zoom), '/', str(tile_x), '/', str(tile_y)])
-                            out_file = ''.join([out_directory, '/location_datetime.feather'])
-                            new_df = new_df.astype({'id': 'int32'})
+                        unique_key_list = convert_cat_subcat_df['location_datetime'].values.tolist()
+                        new_df = tile_df[(new_datetime <= tile_df['datetime']) & (tile_df['datetime'] < new_datetime + timedelta(minutes=10))].dropna(subset=unique_key_list).dropna(axis=1, how='all')
+                        new_df_len = len(new_df.index)
+                        if new_df_len > 0:
+                            out_directory = ''.join([out_dir, '/', convert_cat_subcat, '/', str(new_datetime.year).zfill(4), '/', str(new_datetime.month).zfill(2), str(new_datetime.day).zfill(2), '/', str(new_datetime.hour).zfill(2), str(math.floor(new_datetime.minute / 10)), '0/', str(zoom), '/', str(tile_x), '/', str(tile_y)])
                             new_df.insert(1, 'indicator', ord(cccc[0]) * 1000000 + ord(cccc[1]) * 10000 + ord(cccc[2]) * 100 + ord(cccc[3]))
                             new_df = new_df.astype({'indicator': 'int32'})
-                            etfo_df = pd.to_datetime(new_df['datetime']) - pd.offsets.Second(created_second)
-                            etfo_df = - etfo_df.map(pd.Timestamp.timestamp).astype(int)
-                            new_df.insert(0, 'elapsed time [s]', etfo_df)
-                            new_df = new_df.astype({'elapsed time [s]': 'int32'})
-                            etfo_list = etfo_df.tolist()
-                            tmp_id_etfo_dict = {}
+                            unique_key_list.insert(0, 'indicator')
                             old_df = new_df.iloc[0:0]
-                            for index, id in enumerate(new_df['id'].tolist()):
-                                tmp_id_etfo_dict[id] = etfo_list[index]
-                            new_id_etfo_dict[(tile_x,  tile_y, new_datetime)] = tmp_id_etfo_dict
+                            out_file = ''.join([out_directory, '/location_datetime.feather'])
                             if out_file in out_file_dict:
                                 old_df = out_file_dict[out_file]
                             elif os.path.exists(out_file):
-                                old_df = pa.ipc.open_file(out_file).read_pandas()
-                            if len(old_df['id'].index) > 0:
-                                concat_df = pd.concat([old_df, new_df], ignore_index=True)
-                                concat_df = concat_df.astype({'id': 'int32'})
+                                old_df = feather.read_feather(out_file)
+                            old_df_index_list = old_df.index.tolist()
+                            old_df_len = len(old_df_index_list)
+                            if old_df_len > 0:
+                                new_df_index_start = old_df_index_list[-1] + 1
+                                new_df.index = range(new_df_index_start, new_df_index_start + new_df_len)
+                                concat_df = pd.concat([old_df, new_df[unique_key_list]])
                                 concat_df = concat_df.astype({'indicator': 'int32'})
-                                concat_df = concat_df.astype({'elapsed time [s]': 'int32'})
                                 unique_key_list = concat_df.columns.values.tolist()
-                                unique_key_list.pop(2)#del id
-                                unique_key_list.pop(0)#del etfo
-                                duplicated = concat_df.duplicated(subset=unique_key_list, keep='last')
-                                del_etfo_id_dict[(tile_x, tile_y, new_datetime)] = concat_df[duplicated][['elapsed time [s]', 'id']]
-                                concat_df.drop_duplicates(subset=unique_key_list, keep='last', inplace=True)
-                                if len(concat_df['id'].index) > 0:
-                                    out_file_dict[out_file] = concat_df
-                                else:
-                                    out_file_dict.pop(out_file)
-                            else:
-                                out_file_dict[out_file] = new_df
-        else:
-            prop_match = re.search(r'^.*/' + cccc + '/' + form + '/' + cat_dir + '/' + date_hourminute + '/C_' + creator + '_' + created + '/([^/]*)\.feather$', in_file)
-            if prop_match:
-                prop_short_name = prop_match.group(1)
-                in_df = pa.ipc.open_file(in_file).read_pandas()
-                for tile_x in range(0, 2**(zoom + 1)):
-                    for tile_y in range(0, 2**(zoom)):
-                        for new_datetime in new_datetime_list_dict[tile_x,  tile_y]:
-                            if len(new_id_etfo_dict[tile_x,  tile_y, new_datetime]) > 0:
-                                intersection_id_list = list(set(new_id_etfo_dict[(tile_x,  tile_y, new_datetime)].keys()) & set(in_df['id'].tolist()))
-                                new_df = in_df[in_df['id'].isin(intersection_id_list)]
-                                if len(new_df['id'].index) > 0:
-                                    out_directory = ''.join([out_dir, '/', form, '/', cat_dir, '/', str(new_datetime.year).zfill(4), '/', str(new_datetime.month).zfill(2), str(new_datetime.day).zfill(2), '/', str(new_datetime.hour).zfill(2), str(math.floor(new_datetime.minute / 10)), '0/', str(zoom), '/', str(tile_x), '/', str(tile_y)])
-                                    out_file = ''.join([out_directory, '/', prop_short_name, '.feather'])
-                                    new_df = new_df.astype({'id': 'int32'})
-                                    new_df.insert(1, 'indicator', ord(cccc[0]) * 1000000 + ord(cccc[1]) * 10000 + ord(cccc[2]) * 100 + ord(cccc[3]))
-                                    new_df = new_df.astype({'indicator': 'int32'})
-                                    tmp_etfo_list = []
-                                    tmp_id_etfo_dict = new_id_etfo_dict[(tile_x,  tile_y, new_datetime)]
-                                    for id in new_df['id'].tolist():
-                                        tmp_etfo_list.append(tmp_id_etfo_dict[id])
-                                    new_df.insert(0, 'elapsed time [s]', tmp_etfo_list)
-                                    new_df = new_df.astype({'elapsed time [s]': 'int32'})
-                                    old_df = new_df.iloc[0:0]
-                                    if out_file in out_file_dict:
-                                        old_df = out_file_dict[out_file]
-                                    elif os.path.exists(out_file):
-                                        old_df = pa.ipc.open_file(out_file).read_pandas()
-                                    if len(old_df['id'].index) > 0:
-                                        concat_df = pd.concat([old_df, new_df], ignore_index=True)
-                                        concat_df = concat_df.astype({'id': 'int32'})
-                                        concat_df = concat_df.astype({'indicator': 'int32'})
-                                        concat_df = concat_df.astype({'elapsed time [s]': 'int32'})
-                                        del_index_list = []
-                                        if (tile_x,  tile_y, new_datetime) in del_etfo_id_dict:
-                                            for del_etfo_id in del_etfo_id_dict[(tile_x,  tile_y, new_datetime)].itertuples():
-                                                for del_index in concat_df.index[(concat_df['elapsed time [s]'] == del_etfo_id[1]) & (concat_df['id'] == del_etfo_id[2])]:
-                                                    del_index_list.append(del_index)
-                                        if len(del_index_list) > 0:
-                                            concat_df.drop(concat_df.index[del_index_list], inplace=True)
-                                        unique_key_list = new_df.columns.values.tolist()
-                                        concat_df.drop_duplicates(subset=unique_key_list, keep='last', inplace=True)
-                                        if len(concat_df['id'].index) > 0:
-                                            out_file_dict[out_file] = concat_df
+                                duplicated = concat_df.duplicated(keep='first')
+                                keep_index_list = concat_df[duplicated].index.tolist()
+                                out_file_dict[out_file] = concat_df.drop_duplicates(keep='last')
+                                for column in new_df.columns:
+                                    if not column in unique_key_list:
+                                        new_column_df = new_df[[column]]
+                                        old_column_df = new_column_df.iloc[0:0]
+                                        out_file = ''.join([out_directory, '/', column.split('[')[0].strip(' ').replace(' ', '_'), '.feather'])
+                                        if out_file in out_file_dict:
+                                            old_column_df = out_file_dict[out_file]
+                                        elif os.path.exists(out_file):
+                                            old_column_df = feather.read_feather(out_file)
+                                        old_column_df_len = len(old_column_df.index)
+                                        if old_column_df_len > 0:
+                                            concat_df = pd.concat([old_column_df, new_column_df])
                                         else:
-                                            out_file_dict.pop(out_file)
-                                    else:
-                                        out_file_dict[out_file] = new_df
+                                            concat_df = new_column_df
+                                        out_file_dict[out_file] = concat_df.loc[keep_index_list].dropna()
+                            else:
+                                out_file_dict[out_file] = new_df[unique_key_list]
+                                for column in new_df.columns:
+                                    if not column in unique_key_list:
+                                        out_file = ''.join([out_directory, '/', column.split('[')[0].strip(' ').replace(' ', '_'), '.feather'])
+                                        out_file_dict[out_file] = new_df[[column]].dropna()
     for out_file, out_df in out_file_dict.items():
-        if len(out_df['id'].index) > 0:
+        if len(out_df.index) > 0:
             os.makedirs(os.path.dirname(out_file), exist_ok=True)
             with open(out_file, 'bw') as out_f:
-                feather.write_feather(out_df, out_f, compression='uncompressed')
+                table = pa.Table.from_pandas(out_df, preserve_index=True)
+                feather.write_feather(table, out_f, compression='zstd')
                 print(out_file, file=out_list_file)
 
 def main():
