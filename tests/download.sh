23f3c7bcc39a361bc1c961965d8d8a1b2c3f3e5d
noyori <noyori@138.195.32.202.bf.2iij.net>
2021-06-25 02:15:42 +0000

    commit

--- tests/download.sh
+++ tests/download.sh
@@ -73,7 +73,7 @@ if test ! -s download_${priority}/aria2c.log; then
 fi
 grep "ETag:" download_${priority}/aria2c.log | tail -1 | cut -d' ' -f2 > download_${priority}/etag.txt
 if test -s download_${priority}/created.txt; then
-  cat download_${priority}/created.txt | grep -v "/A_ISXX[0-9][0-9]EUSR" | grep -v "/A_P" | sort -u > download_${priority}/created.txt.tmp
+  cat download_${priority}/created.txt | grep -v "/A_P" | sort -u > download_${priority}/created.txt.tmp
   mv -f download_${priority}/created.txt.tmp download_${priority}/created.txt
   if test ! -s download_${priority}/created.txt; then
     exit 0
