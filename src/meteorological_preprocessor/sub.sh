346db3474a471d332ea022fbd564c529109c8319
public.tatsuya.noyori <public.tatsuya.noyori@gmail.com>
2021-07-16 08:01:08 +0900

    commit

--- src/meteorological_preprocessor/sub.sh
+++ src/meteorological_preprocessor/sub.sh
@@ -190,16 +190,20 @@ subscribe() {
           timeout -k 3 ${rclone_timeout} rclone copy --bwlimit ${bandwidth_limit_k_bytes_per_s} --checksum --contimeout ${timeout} --files-from-raw ${source_work_directory}/filtered_newly_created_file.tmp --local-no-set-modtime --log-file ${source_work_directory}/info_log.tmp --log-level DEBUG --low-level-retries 3 --no-traverse --retries 3 --s3-no-check-bucket --s3-no-head --s3-no-head-object --stats 0 --timeout ${timeout} --transfers ${parallel} ${source_rclone_remote_bucket} ${local_work_directory}
           exit_code=$?
           set -e
-          if test ${exit_code} -ne 0; then
+          if test ${exit_code} -eq 0; then
+            set +e
+            grep -E "^(.* DEBUG *: *[^ ]* *:.* Unchanged skipping.*|.* INFO *: *[^ ]* *:.* Copied .*)$" ${source_work_directory}/info_log.tmp | sed -e "s|^.* DEBUG *: *\([^ ]*\) *:.* Unchanged skipping.*$|/\1|g" -e "s|^.* INFO *: *\([^ ]*\) *:.* Copied .*$|/\1|g" -e 's|^/||g' | grep -v '^ *$' | sort -u >> ${work_directory}/processed_file.txt
+            set -e
+          else
             set +e
             grep -F ERROR ${source_work_directory}/info_log.tmp >&2
             set -e
             echo "ERROR: ${exit_code}: can not get file from ${source_rclone_remote_bucket} ${txt_or_bin}." >&2
             continue
           fi
-          set +e
-          grep -E "^(.* DEBUG *: *[^ ]* *:.* Unchanged skipping.*|.* INFO *: *[^ ]* *:.* Copied .*)$" ${source_work_directory}/info_log.tmp | sed -e "s|^.* DEBUG *: *\([^ ]*\) *:.* Unchanged skipping.*$|/\1|g" -e "s|^.* INFO *: *\([^ ]*\) *:.* Copied .*$|/\1|g" -e 's|^/||g' | grep -v '^ *$' | sort -u >> ${work_directory}/processed_file.txt
-          set -e
+        fi
+        if test ${exit_code} -eq 0; then
+          mv -f ${source_work_directory}/${pubsub_index_directory}_new_index.tmp ${source_work_directory}/${pubsub_index_directory}_index.txt
         fi
       fi
     fi
