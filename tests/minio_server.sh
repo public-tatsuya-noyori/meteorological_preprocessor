83ab673bd9a733b2bfb066fe5c63a931b42cfc98
noyori <noyori@138.195.32.202.bf.2iij.net>
2021-08-02 02:02:18 +0000

    commit

---
+++ tests/minio_server.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+uid=`grep MINIO_ROOT_USER /home/noyori/minio_uid_passwd.txt | cut -d':' -f2`
+passwd=`grep MINIO_ROOT_PASSWORD /home/noyori/minio_uid_passwd.txt | cut -d':' -f2`
+MINIO_BROWSER=off MINIO_ROOT_USER=${uid} MINIO_ROOT_PASSWORD=${passwd} /home/noyori/minio server /home/noyori/minio_data 1>> /home/noyori/minio.log 2>>/home/noyori/minio.log &
+echo $! > /home/noyori/minio_pid.txt
+sleep 10
+./mc admin update minio/
