915cce34336d35ca2c09e5876f83c6ecc92b3446
noyori <noyori@138.195.32.202.bf.2iij.net>
2021-09-26 23:31:56 +0000

    commit

--- tests/make_checksum_arrow_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import pyarrow as pa
-from pyarrow import feather
-
-with open('checksum.feather', 'bw') as out_f:
-    property_batch = pa.record_batch([[], []], names=['mtime', 'checksum'])
-    property_table = pa.Table.from_batches([property_batch])
-    feather.write_feather(property_table, out_f, compression='zstd')
