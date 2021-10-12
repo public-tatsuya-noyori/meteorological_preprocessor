fdb61f858c07c542a4fb5949f2b436a4a2bdc0b1
public.tatsuya.noyori <public.tatsuya.noyori@gmail.com>
2021-10-12 11:47:28 +0900

    commit

--- src/meteorological_preprocessor/aws/move_4PubSub_4Search_sub_function.sh
+++ src/meteorological_preprocessor/aws/move_4PubSub_4Search_sub_function.sh
@@ -14,12 +14,13 @@ function handler () {
   fi
   rclone_remote_bucket=`cat $HOME/.config/rclone/my_remote_bucket.txt | grep ^rclone_remote_bucket_main_sub | cut -d= -f2 | sed -e 's| ||g' | cut -d';' -f${main_sub_num}`
   center_id=`cat $HOME/.config/rclone/my_remote_bucket.txt | grep ^center_id | cut -d= -f2 | sed -e 's| ||g'`
-  pub_clone_directory=4PubClone
-  pub_clone_work_directory=$HOME/${pub_clone_directory}
+  work_directory=$HOME/${function}
   IFS=$'\n'
   rc=0
-  commands_args="./move_4PubSub_4Search.sh --no_check_pid --config rclone.conf ${pub_clone_work_directory} ${center_id} txt ${rclone_remote_bucket}
-./move_4PubSub_4Search.sh --no_check_pid --config rclone.conf ${pub_clone_work_directory} ${center_id} bin ${rclone_remote_bucket}"
+  rm -rf ${work_directory}
+  mkdir -p ${work_directory}
+  commands_args="./move_4PubSub_4Search.sh --no_check_pid --config rclone.conf ${work_directory} ${center_id} txt ${rclone_remote_bucket}
+./move_4PubSub_4Search.sh --no_check_pid --config rclone.conf ${work_directory} ${center_id} bin ${rclone_remote_bucket}"
   for command_args in `echo "${commands_args}"`; do
     echo ${command_args[@]} | xargs -r -I {} sh -c "{}" &
     pids+=($!)
